16fec096420d7d4dcc958299c2dd010cf1dbd3ea
ichik <ichik@yandex.ru>
2011-04-11 18:48:02 +0400

    Fixing shit

--- settings.lua
+++ settings.lua
@@ -24,47 +24,47 @@ Filger_Spells = {
 				setPoint = { "CENTER", "UIParent", "CENTER", 0, 100 },
 				
 				-- Lichborne
-				{ spellID = 49039, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 49039, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Divine Plea
-				{ spellID = 54428, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 54428, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Hand of Protection
-				{ spellID = 1022, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 1022, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Pain Suppression
-				{ spellID = 33206, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 33206, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Divine Shield
-				{ spellID = 642, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 642, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Shield Wall
-				{ spellID = 871, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 871, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster = "all"},
 				-- Icebound Fortitude
-				{ spellID = 48792, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 48792, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Barkskin
-				{ spellID = 22812, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 22812, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Dispersion
-				{ spellID = 47585, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 47585, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Ice Block
-				{ spellID = 45438, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 45438, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Divine Protection
-				{ spellID = 498, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 498, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Recklesness
-				{ spellID = 1719, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 1719, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Avenging Wrath
-				{ spellID = 31884, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 31884, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Berserk
-				{ spellID = 50334, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 50334, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Master Aura
-				{ spellID = 31821, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 31821, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Deterrence
-				{ spellID = 19263, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 19263, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Strength of Soul
-				{ spellID = 96267, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 96267, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Hand of Sacrifice
-				{ spellID = 6940, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 6940, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- The Beast Within
-				{ spellID = 34471, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 34471, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Evasion
-				{ spellID = 5277, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 5277, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 				-- Unholy Frenzy
-				{ spellID = 49016, size = 48, scale = 1, filter = "BUFF", unitId = "target", },
+				{ spellID = 49016, size = 48, scale = 1, filter = "BUFF", unitId = "target", caster="all"},
 			},
 			{
 				Name = "FOCUS",
@@ -74,9 +74,9 @@ Filger_Spells = {
 				setPoint = { "CENTER", "UIParent", "CENTER", 300, 100 },
 								
 				-- Unholy Frenzy
-				{ spellID = 49016, size = 48, scale = 1, filter = "BUFF", unitId = "focus", },
+				{ spellID = 49016, size = 48, scale = 1, filter = "BUFF", unitId = "focus", caster="all"},
 				-- Icebound Fortitude
-				{ spellID = 48792, size = 48, scale = 1, filter = "BUFF", unitId = "focus", },
+				{ spellID = 48792, size = 48, scale = 1, filter = "BUFF", unitId = "focus", caster="all"},
 			},
 		},
 
